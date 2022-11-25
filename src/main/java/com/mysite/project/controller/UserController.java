95d2038a063e3b001a99c2f88217d646cf6b2c3a
SUJINJEONG012 <peekaboo32@naver.com>
2022-11-25 22:18:06 +0900

    RoleType 추가

--- src/main/java/com/mysite/project/controller/UserController.java
+++ src/main/java/com/mysite/project/controller/UserController.java
@@ -3,15 +3,19 @@ package com.mysite.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//인증이 안된 사용자들이 출입할 수 있는 경로를 /auth/** 허용
+//주소가 / 인 주소를 index.jsp 허용
+//static 아래에 있는 /js/**, /css/**, /images/**
+
 @Controller
 public class UserController {
 	
-	@GetMapping("/user/joinForm")
+	@GetMapping("/auth/joinForm")
 	public String joinForm() {
 		return "/user/join";
 	}
 	
-	@GetMapping("/user/loginForm")
+	@GetMapping("/auth//loginForm")
 	public String loginForm() {
 		return "/user/login";
 	}
