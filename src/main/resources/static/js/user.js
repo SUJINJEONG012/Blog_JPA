c8a1d0a192f5ed20a6681b53eb1bd88437e4acfd
SUJINJEONG012 <peekaboo32@naver.com>
2022-11-25 22:19:01 +0900

    리다이렉트 다시 설정

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,10 @@ let index = {
 		$("#btn-save").on("click", () => { //function를 사용하지않은 이유는 this를 바인딩하기 위해서
 			this.save();
 		});
+		
+		$("#btn-login").on("click", () => { //function를 사용하지않은 이유는 this를 바인딩하기 위해서
+			this.login();
+		});
 	},
 	
 	save: function() {
@@ -12,7 +16,6 @@ let index = {
 			password:$("#password").val(),
 			email:$("#email").val()
 		};
-		
 		//console.log(data);
 		
 		
@@ -28,7 +31,31 @@ let index = {
 		}).done(function(resp){
 			alert("회원가입이 완료되었습니다. ");
 			console.log(resp);
-			//location.href="/";
+			location.href="/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+		
+	},	
+		login: function() {
+		// alert("user의 svae할수있음 ");
+		let data = {
+			username:$("#username").val(), //username이 들고있는값을변수에다가 바인딩을 한다. 
+			password:$("#password").val(),
+			
+		};
+		
+	$.ajax({
+		type:"POST",
+		url:"/api/user/login", //맵핑되는 주소랑 동일
+		data:JSON.stringify(data), 
+		contentType:"application/json; charset=utf-8", //body 데이터가 어떤 타입 인지 
+		dataType:"json" //요청을 서버로 해서 응답이 왔을 때 기본적으로 모든것이 문자열("json이라면 ?")
+		//회원가입 수행 요청 
+		}).done(function(resp){
+			alert("로그인이 완료되었습니다. ");
+			console.log(resp);
+			location.href="/";
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		});
