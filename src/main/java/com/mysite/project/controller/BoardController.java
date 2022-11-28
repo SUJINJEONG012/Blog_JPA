2c8bf5346c2852c7417f899d0861158e5efb478e
SUJINJEONG012 <peekaboo32@naver.com>
2022-11-29 03:48:54 +0900

    페이징처리 추가

--- src/main/java/com/mysite/project/controller/BoardController.java
+++ src/main/java/com/mysite/project/controller/BoardController.java
@@ -1,6 +1,11 @@
 package com.mysite.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +21,8 @@ public class BoardController {
 	
 	@GetMapping({"","/"})
 	//메인페이지로 갈때 데이터를 가져가야한다.
-	public String index(Model model) {
-		model.addAttribute("boards", boardService.boardList());
+	public String index(Model model,@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable) {
+		model.addAttribute("boards", boardService.boardList(pageable));
 		return "index"; //viewResolver 작동 
 	}
 	
--- src/main/java/com/mysite/project/service/BoardService.java
+++ src/main/java/com/mysite/project/service/BoardService.java
@@ -3,6 +3,8 @@ package com.mysite.project.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,8 +37,8 @@ public class BoardService {
 	
 	
 	//글 목록리스트
-	public List<Board> boardList() {	
-		return boardRepository.findAll();
+	public Page<Board> boardList(Pageable pageable) {	
+		return boardRepository.findAll(pageable);
 	}
 
 
