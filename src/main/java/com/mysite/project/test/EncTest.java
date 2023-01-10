0c28d27bcc4cbbb86b5badebec9bb8f2dd5c78c2
SUJINJEONG012 <peekaboo32@naver.com>
2023-01-11 04:35:47 +0900

    해쉬암호화 주석처리

--- src/main/java/com/mysite/project/test/EncTest.java
+++ src/main/java/com/mysite/project/test/EncTest.java
@@ -6,9 +6,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class EncTest {
 
 	
-	@Test
-	public void 해쉬암호화() {
-		String encPassoword = new BCryptPasswordEncoder().encode("1234");
-		System.out.println("encPassoword 암호화  :" +encPassoword  );
-	}
+//	@Test
+//	public void 해쉬암호화() {
+//		String encPassoword = new BCryptPasswordEncoder().encode("1234");
+//		System.out.println("encPassoword 암호화  :" +encPassoword  );
+//	}
 }
